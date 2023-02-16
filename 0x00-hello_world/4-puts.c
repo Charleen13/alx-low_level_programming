b8ddecc121953318f9ac2b29d513f42ae2273eac
Charleen13 <Charleenmpofu33@gmail.com>
2023-02-16 12:31:42 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+
+/**
+ * main -this is the main function
+ *
+ * this is a c program without a printf
+ *
+ *  Return:0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
