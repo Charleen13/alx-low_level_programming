48dffa6f516dff6c3248f37c5e57532201add80b
Charleen13 <Charleenmpofu33@gmail.com>
2023-02-21 18:56:37 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * print_alphabet_x10 - function to print alphabet
+ * Return: Always 0.
+ */
+int main(void)
+{
+     print_alphabet_x10();
+     return (0);
+}
+
