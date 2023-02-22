cb43137a3e7d740aada07516cd7c20ede5c4c73c
Charleen13 <Charleenmpofu33@gmail.com>
2023-02-21 19:10:58 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -2,12 +2,12 @@
 
 /**
  * main - check the code.
- * print_alphabet_x10 - function to print alphabet
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-     print_alphabet_x10();
-     return (0);
+print_alphabet_x10();
+return (0);
 }
 
