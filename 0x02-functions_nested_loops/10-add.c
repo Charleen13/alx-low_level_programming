48efcde7c06adcca8aadafdbc9297ba54d400e7c
Charleen13 <Charleenmpofu33@gmail.com>
2023-02-22 18:45:36 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @a: para 1
+ * @d: para 2
+ *
+ * Return: sum
+ */
+
+int add(int a, int d)
+{
+	return (a + d);
+}
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9x table
+ *
+ * Return: always 0 
+ */
+
+void times_table(void)
+{
+
+}
