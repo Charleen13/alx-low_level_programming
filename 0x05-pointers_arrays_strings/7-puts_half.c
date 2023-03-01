2093c3c6a8a79368f0d5a29c3f1b1220cc7bb93c
Charleen13 <Charleenmpofu33@gmail.com>
2023-02-28 17:12:04 -0800

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 	for (a = 0; str[a] != '\0'; a++)
 		longi++;
 	n = (longi / 2);
-	if ((longi % 2) == 1)
+	 if ((longi % 2) == 1)
 			n = ((longi + 1) / 2);
 	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
