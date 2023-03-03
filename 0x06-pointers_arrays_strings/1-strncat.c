398427875cad24672bcb6e775c1682538f1f80c3
Charleen13 <Charleenmpofu33@gmail.com>
2023-03-02 18:46:44 -0800

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings
+* @dest: input value
+* @src: input value
+* @n: number of characters to be append
+* Return: concatenated string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i < n ; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
