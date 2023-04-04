e3a44137aaeaec7d3e78d4515d45398d28854baf
Charleen13 <Charleenmpofu33@gmail.com>
2023-04-04 04:03:35 -0700

    a function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,8 +12,8 @@
  */
 typedef struct listint_s
 {
- int n;
- struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -30,6 +30,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-int _putchar(char c);
+int _putchar(char c)i
 #endif
 
